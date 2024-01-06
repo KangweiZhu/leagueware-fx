f29f9e584465e9a5eacb4fdfce73de5d15f7dc47
Kangwei Zhu <kz245@rutgers.edu>
2024-01-06 05:01:33 -0500

    添加注释，并修改控制台输出

--- src/main/java/com/anicaaz/leaguewarefx/LeagueWareFXStarter.java
+++ src/main/java/com/anicaaz/leaguewarefx/LeagueWareFXStarter.java
@@ -4,6 +4,7 @@ import com.anicaaz.leaguewarefx.constants.RequestConstants;
 import com.anicaaz.leaguewarefx.utils.EncodingUtil;
 import com.anicaaz.leaguewarefx.utils.ExecuteCommand;
 import com.anicaaz.leaguewarefx.utils.HttpsUtils;
+import com.anicaaz.leaguewarefx.utils.LogUtil;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -12,7 +13,10 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Map;
 
-
+/**
+ * LeagueWare程序的主入口，负责在视图显示前，获取必要的信息。如token, 端口，用户信息
+ * @author anicaa
+ */
 public class LeagueWareFXStarter extends Application {
     public static String remotingAuthToken;
     public static String appPort;
@@ -21,27 +25,42 @@ public class LeagueWareFXStarter extends Application {
     public static Integer profileIconId;
     public static Integer summonerLevel;
 
+    /**
+     * 从main-view中加载主页，并设置窗口属性。
+     * todo: 未检测到LCU进程时，一直处于一个等待界面，而不是直接报错。
+     *
+     * @param stage fxml文件对应的stage.
+     * @throws IOException
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(LeagueWareFXStarter.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 700)    ;
+        Scene scene = new Scene(fxmlLoader.load(), 400, 700);
         stage.setTitle("LeagueWare");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
+    /**
+     * 主入口。获取必要信息，并调用javafx的launch方法来启动app.
+     * @param args
+     */
     public static void main(String[] args) {
         getConnectionPrerequisites();
         getSummonerInfo();
         launch();
     }
 
+    /**
+     * 获取获取token和端口号
+     */
     public static void getConnectionPrerequisites() {
         String rawToken = ExecuteCommand.getRemoteAuthToken();
         remotingAuthToken = "Basic " + EncodingUtil.encodeToBase64("riot:" + rawToken);
         appPort = ExecuteCommand.getAppPort();
-        System.out.println(remotingAuthToken);
-        System.out.println(appPort);
+        LogUtil.log(LeagueWareFXStarter.class.getName(), "getConnectionPrerequisites", "remotingAuthToken: " + remotingAuthToken);
+        LogUtil.log(LeagueWareFXStarter.class.getName(), "getConnectionPrerequisites", "appPort: " + appPort);
     }
 
     public static void getSummonerInfo() {
